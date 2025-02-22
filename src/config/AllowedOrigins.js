c00d26805fad8561ccf7a1d36848c04b7e5d94a2
dkikowe <dkikowe@gmail.com>
2025-02-23 00:40:09 +0500

    fss

--- src/config/AllowedOrigins.js
+++ src/config/AllowedOrigins.js
@@ -4,7 +4,7 @@ const allowedOrigins = [
   "http://localhost:5050",
   "http://localhost:3001",
   "http://localhost:3002",
-  "http://localhost:2024",
+
   "http://localhost:5173",
   "https://mel-auto-main.vercel.app",
   "https://mel-auto-admin.vercel.app",
